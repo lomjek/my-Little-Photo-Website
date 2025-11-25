7000e9f1f380c4f6e05665d0ba50188d8376fc4b
lomjek <leondergrunder@gmail.com>
2025-11-25 15:56:32 +0100

    Fixed IOD_link_bug

--- libs/iod.php
+++ libs/iod.php
@@ -1,64 +1,72 @@
 <?php
+
 /*****************************************************/
 /*  This file is part of 'my Little Photo Website'   */
 /* It is published on github under the MIT License:  */
 /* https://github.com/lomjek/my-Little-Photo-Website */
 /*****************************************************/
 
-function get_rand_img_path(){
-	//Set the seed
-	$currentDate = date("Ymd");
-	mt_srand($currentDate);
-	//Set the directory
-	$directories = get_collections();
-	$randomNumber = mt_rand(1, count($directories));
-	$directory = $directories[$randomNumber - 1];
-	//Set the image
-	$files = scandir($_SERVER['DOCUMENT_ROOT'] . '/photos/' . $directory);
-	foreach ($files as $file){
-		if (str_starts_with($file, '.')){
-			$files = array_diff($files, [$file]);
-		}
-	}
-	$files = array_values($files);
-	$randomFileIndex = mt_rand(1, count($files));
-	$file = $files[$randomFileIndex - 1];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/libs/collections.php';
+
+function get_rand_img_path()
+{
+    //Set the seed
+    $currentDate = date("Ymd");
+    mt_srand($currentDate);
+    //Set the directory
+    $directories = get_collections();
+    $randomNumber = mt_rand(1, count($directories));
+    $directory = $directories[$randomNumber - 1];
+    //Set the image
+    $files = scandir($_SERVER['DOCUMENT_ROOT'] . '/photos/' . $directory);
+    foreach ($files as $file) {
+        if (str_starts_with($file, '.')) {
+            $files = array_diff($files, [$file]);
+        }
+    }
+    $files = array_values($files);
+    $randomFileIndex = mt_rand(1, count($files));
+    $file = $files[$randomFileIndex - 1];
 
     $file = 'photos/' . $directory . '/' . $file;
 
-	return $file;
+    return $file;
 }
 
 
-function needs_reload(){
+function needs_reload()
+{
     $reload_file = $_SERVER['DOCUMENT_ROOT'] . '/data/iod.conf';
-    if (!file_exists($reload_file)){
+    if (!file_exists($reload_file)) {
         file_put_contents($reload_file, "\n");
         return true;
     }
     $file_contents = file_get_contents($reload_file);
     $line = explode("\n", $file_contents)[0];
-    if (trim($line) == date("Ymd")){
+    if (trim($line) == date("Ymd")) {
         return false;
     } else {
         return true;
     }
 }
 
-function override_iod(){
+function override_iod()
+{
     $reload_file = $_SERVER['DOCUMENT_ROOT'] . '/data/iod.conf';
     file_put_contents($reload_file, date("Ymd") . "\n" . get_rand_img_path(false));
 }
 
-function get_iod_from_file(){
+function get_iod_from_file()
+{
     $reload_file = $_SERVER['DOCUMENT_ROOT'] . '/data/iod.conf';
     $file_contents = file_get_contents($reload_file);
     $line = explode("\n", $file_contents)[1];
     return trim($line);
 }
 
-function get_iod(){
-    if (needs_reload()){
+function get_iod()
+{
+    if (needs_reload()) {
         override_iod();
         return get_iod();
     } else {
@@ -75,8 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && realpath(__FILE__) === realpath($_S
     header('Content-Type: application/x-www-form-urlencoded');
     echo $urlencoded_data;
     exit();
-} elseif (realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])){
+} elseif (realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
     echo "This file cannot be run directly.";
     exit();
 }
-?>
--- update/collection/main.js
+++ update/collection/main.js
@@ -4,14 +4,14 @@
 /* https://github.com/lomjek/my-Little-Photo-Website */
 /*****************************************************/
 
-function change_bg_color(){
+function change_bg_color() {
     const allBodyElements = document.body.querySelectorAll('*');
     allBodyElements.forEach(element => {
         element.style.setProperty("background-color", document.getElementById("bcolor").value, "important");
     });
     document.body.style.setProperty("background-color", document.getElementById("bcolor").value, "important");
 }
-function change_t_col(){
+function change_t_col() {
     const allBodyElements = document.body.querySelectorAll('*');
     allBodyElements.forEach(element => {
         element.style.setProperty("color", document.getElementById("tcolor").value, "important");
@@ -19,14 +19,14 @@ function change_t_col(){
 }
 
 //input
-function leave(){
+function leave() {
     window.location.assign('/update/');
 }
-async function sendRequest(urlencoded_data){
+async function sendRequest(urlencoded_data) {
     try {
         const response = await fetch('/libs/collections.php', {
             method: 'POST',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: urlencoded_data
         })
         console.log(response);
@@ -41,37 +41,37 @@ async function sendRequest(urlencoded_data){
         return false;
     }
 }
-function rename(){
+function rename() {
     const formData = new URLSearchParams();
     formData.append('a', 'rename');
     formData.append('c', original_name);
     formData.append('d', document.getElementById('Title').value);
-    if (sendRequest(formData)){
+    if (sendRequest(formData)) {
         original_name = document.getElementById('Title').value
     }
 }
-function save_description(){
+function save_description() {
     const formData = 'func=set_collection_description&&collection=' + encodeURIComponent(original_name.replaceAll(" ", "-")) + '&&description=' + encodeURIComponent(document.getElementById('desc').value);
     sendRequest(formData);
 }
-function save_colors(){
+function save_colors() {
     let colors = [document.getElementById('bcolor').value, document.getElementById('tcolor').value];
     let formData = 'func=set_collection_colors&&collection=' + encodeURIComponent(original_name.replaceAll(" ", "-")) + '&&bg_color=' + encodeURIComponent(colors[0]) + '&&text_color=' + encodeURIComponent(colors[1]);
     sendRequest(formData);
 }
-function set_visibility(){
+function set_visibility() {
     const formData = 'func=set_collection_visibility&&collection=' + encodeURIComponent(original_name.replaceAll(" ", "-")) + '&&visibility=' + encodeURIComponent(document.getElementById('visibility').value);
     sendRequest(formData)
 }
-async function ask_delete(){
+async function ask_delete() {
     const userChoice = confirm("Dali ste sigurni, da bi izbrisali ovaj skup?\nJednom kad ga nema, ga nema...");
     var send_thing = 'func=delete_collection&&collection=' + original_name.replaceAll(" ", "-");
     console.log(send_thing);
-    if (sendRequest(send_thing)){
+    if (sendRequest(send_thing)) {
         window.location = '/update/';
     }
 }
 //When page loaded...
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     original_name = document.getElementById('Title').value;
 });
