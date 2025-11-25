3ad265b7575a4bd1f31237bf1eb31985ffd86a1e
lomjek <leondergrunder@gmail.com>
2025-11-25 15:56:32 +0100

    Fixed IOD_link_bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 /* https://github.com/lomjek/my-Little-Photo-Website */
 /*****************************************************/
 
-async function get_IOD() {
+async function get_IOD() { //photo
     var iod_link = document.getElementById('iod_link');
     var iod_img = document.getElementById('iod_img');
 
@@ -21,15 +21,20 @@ async function get_IOD() {
         const params = new URLSearchParams(urlEncodedString);
         const receivedString = params.get('data');
 
-        iod_link.onclick = function() {
-            window.location.assign(receivedString);
+        var iod_array = receivedString.split('/');
+        iod_array[0] = iod_array[0].slice(0, -1)
+        iod_array[2] = ".t_" + iod_array[2];
+        let iod_url = iod_array.join("/");
+
+        iod_link.onclick = function () {
+            window.location.assign(iod_url);
         };
-        iod_img.onclick = function() {
-            window.location.assign(receivedString);
+        iod_img.onclick = function () {
+            window.location.assign(iod_url);
         };
-        
+
         iod_img.src = receivedString;
-        
+
     } catch (error) {
         console.error('Fetch error:', error);
     }
@@ -48,15 +53,15 @@ async function get_collections() {
             },
             body: new URLSearchParams(data).toString()
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(data => {
-            collection_container.innerHTML = data;
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.text();
+            })
+            .then(data => {
+                collection_container.innerHTML = data;
+            });
     } catch (error) {
         console.error('Fetch error:', error);
     }
