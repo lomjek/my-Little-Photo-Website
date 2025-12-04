55548782a52c2e7bc17ac2d234fc183f4e12cdd2
lomjek <leondergrunder@gmail.com>
2025-12-04 11:09:30 +0100

    File Uploading

--- update/images/index.js
+++ update/images/index.js
@@ -4,14 +4,15 @@
 /* https://github.com/lomjek/my-Little-Photo-Website */
 /*****************************************************/
 
-function Upload(){
+function Upload() {
     document.getElementById("files").disabled = true;
     document.getElementById("skupovi").disabled = true;
-    document.getElementById("process").disabled = true;
 
     const input = document.getElementById('files');
     const files = input.files;
-    
+
+    console.log("I live");
+
     if (files.length === 0) {
         alert('Please select at least one file.');
         document.getElementById("files").disabled = false;
@@ -23,45 +24,25 @@ function Upload(){
     for (let i = 0; i < files.length; i++) {
         formData.append('files[]', files[i]);
     }
-    formData.append('collection', document.getElementById("skupovi").value);
 
     const xhr = new XMLHttpRequest();
-    xhr.upload.addEventListener('progress', function(e) {
+    xhr.upload.addEventListener('progress', function (e) {
         if (e.lengthComputable) {
             const percentComplete = (e.loaded / e.total) * 100;
             document.getElementById('progressBar').value = percentComplete;
         }
     });
-    xhr.onload = function() {
+    xhr.onload = function () {
         if (xhr.status === 200 || xhr.status == 202 || xhr.status == 201) {
-            document.getElementById("process").disabled = false;
             console.log(xhr.responseText)
+            //processImages(xhr.responseText)
         } else {
             console.error('Error:', xhr.statusText);
             alert('Error uploading files.');
         }
     };
-    xhr.open('POST', '/update/images/index.php', true);
+    xhr.open('POST', '/libs/upload.php', true);
     document.getElementById("Upload").style.display = 'none';
     document.getElementById('uploading').style.display = 'block';
     xhr.send(formData);
-}
-function sendProcess(){
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            document.getElementById("process").disabled = false;
-            console.log(xhr.responseText)
-            alert("Done processing images!")
-        } else if (xhr.status == 409) {
-            alert("An instance of the converter already running.")
-        } else {
-            console.error('Error:', xhr.statusText);
-            alert('Error uploading files.');
-        }
-    };
-    xhr.open('PUT', '/update/images/index.php', true);
-    xhr.send();
-    console.log("Sent!!!")
-    alert("Processing Images Started, you may leave the site now...")
 }
